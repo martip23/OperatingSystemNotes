75ccc052eadadcc9fbc7035c32022eaa170dec16
martip23 <patrickmartinez77@gmail.com>
2018-07-25 08:08:13 -0700

    Update for restart

---
+++ Homework/HW2/classwork.c
@@ -0,0 +1,18 @@
+for (j = 0; j <= pipes; j++) {
+	if (pid>0)
+	{
+		if (j>0)
+		{
+			close(l_fd[0])
+			close(l_fd[1])
+		}
+		l_fd[0] = r_fd[0];
+		l_fd[1] = r_fd[1];
+	}
+}
+if (pid==0) {
+
+
+
+	exevp();
+}
--- Homework/HW2/lab2.c
+++ Homework/HW2/lab2.c
@@ -10,8 +10,6 @@ int main()
 char *path, *argv[20], buf[80], n, *p;
 
 int m, status, inword, continu;
-int pid = -1;
-int pid2 = -1;
 
 while(1) {
 
@@ -31,78 +29,18 @@ while ( ( n = getchar() ) != '\n'  || continu ) {
 		inword = 0;
 		*p++ = 0;
 	    } 
-
-	}	
-	else if ( n == '<') {
-		printf("< DETECTED\n");
-
-
-
-
-	} else if ( n == '|') {
-		printf("| DETECTED\n");
-		int pid;
-		int fd[2];
-		
-		// Create pipe
-		if (pipe(fd) == -1)
-			printf("ERROR! PIPE NOT CREATED for some reason");
-
-		// Create child
-		pid = fork();
-
-		if (pid < 0)
-		{
-			printf("ERROR! Child not created for some reason\n");
-		}
-
-		// Child create grandchild
-		else if (pid == 0) {
-			pid2 = fork();
-			wait(NULL);
-
-			// Child read from pipe
-			if (pid2 > 0) {
-				close(0);
-				dup(fd[0]);
-				close(fd[0]);
-				close(fd[1]);
-
-				m = 0;
-				*p++;
-
-				wait(NULL);
-
-			}
-			// Grandchild write to pipe
-			else if (pid2 == 0) {
-				close(1);
-				dup(fd[1]);
-				close(fd[1]);
-				close(fd[0]);				
-
-				break;
-			}
-		} else break;
-
-	} else if ( n == '>') {
-		printf("> DETECTED\n");
 	} else if ( n == '\n' ) {
 		continu = 0;
 	} else if ( n == '\\' && !inword ) {
 		continu = 1;
 	} else {
-
 	    if ( !inword ) {
-
 		 inword = 1;
 		 argv[m++] = p;
 		 *p++ = n;
 	     }
-
 	     else 
 		 *p++ = n;
-
 	}
 }	
 
@@ -111,14 +49,70 @@ while ( ( n = getchar() ) != '\n'  || continu ) {
 argv[m] = 0;
 
 //printf("Argv[0] = %s\n", argv[0]);
+for (int i = 0; i < (m-1); i++) {
+	printf("i=%d\n", i);
+	printf("m=%d\n", m);
+	char *argvp[20];
+	printf("%s\n", argv[i]);
+	// if((strcmp(argv[i], "|") != 0) && ((strcmp(argv[i],"<") != 0)) 
+	// 	&& (strcmp(argv[i], ">") != 0)) {
+	// 	strcpy(argvp[i], argv[i]);
+	// 	printf("STRCPY CALLED\n");
+	// }
+	if (strcmp(argv[i], "|") == 0){
+		char *argvc[20];
+		printf("i=%d\n", i);
+
+		for (int j = (i + 1), c = 0; j < m; j++) {
+			printf("c=%d\n", c);
+			printf("j=%d\n", j);
+			strcpy(argvc[c], argv[j]);
+			c++;
+		}
+ 
+		int pid;
+		int fd[2];
+
+		printf("%s\n", "Making pipe");
+		if (pipe(fd) == -1)
+			printf("Pipe not created");
+
+		pid = fork();
+
+		if (pid < 0)
+			printf("Child not created");
+
+		/**
+		Set parent to push to pipe
+		**/
+		else if (pid > 0) {
+			close(1);
+			dup(fd[1]);
+			close(fd[1]);
+			close(fd[0]);
+		}
+		/**
+		Set child to read from pipe and run
+		**/
+		else {
+			close(0);
+			dup(fd[0]);
+			close(fd[0]);
+			close(fd[1]);
+
+			wait(NULL);
+
+			if(execvp(argvc[0], argvc) == -1)
+				printf("Error with child pipe\n");
+		}
+	}
+	printf("OUT OF FOR\n");
+}
+printf("Out Of Loop\n");
 
 if ( strcmp(argv[0],"quit") == 0 ) exit (0);
 
-if (pid == 0 || pid2 == 0) {
-//	printf("PID(CH): %d\n", getpid());
- 	execvp( argv[0], argv);
- 	printf("Didn't exec\n");
-} else if ( fork() == 0 )
+if ( fork() == 0 )
 
  	{
 // 	printf("PID: %d\n", getpid());
