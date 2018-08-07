1a6d7976104856d17dd2daa3e3932603d603f571
martip23 <patrickmartinez77@gmail.com>
2018-08-07 17:54:15 -0500

    finished hw 4

--- Homework/HW4/paging.c
+++ Homework/HW4/paging.c
@@ -27,6 +27,12 @@ int max (int a, int b) {
 	return b;
 }
 
+int min (int a, int b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
 int main () {
 	int refStringLen = -1, numFrames = -1, memory[30], refString[30];
 
@@ -44,7 +50,10 @@ int main () {
 
 // Create reference string 
 	for (int i = 0; i < refStringLen; i++) {
-			refString[i] = ((rand() % 5) + 1);
+			int randInt = ((rand() % 5) + 1);
+			if (randInt != refString[i - 1])
+				refString[i] = randInt;
+			else i--;
 	}
 
 // Set bit after last item to 0 or NULL.
@@ -116,6 +125,7 @@ int main () {
 			for (int l = 0; l < numFrames; l++) {
 				if (memory[l] == outVal) {
 					memory[l] = refString[j];
+					break;
 				}
 			}
 //			printf("j = %d ref = %d\n", i, refString[j]);
@@ -125,7 +135,71 @@ int main () {
 	}
 	printf("PAGE FAULTS: %d\n", faults);
 
+// LRU algorithm. Use stack.
+	printf("\n***LRU algorithm***\n");
+	// While there is an empty frame, and reference not in list
+	// place a page into it. Ties do not occur in LRU so it is 
+	// not addressed.
 
+// Set all frames to 0, sting ends at -1
+	for (int i = 0; i < numFrames; i++)
+		memory[i] = 0;
+// Reset faults counter
+	faults = 0;
+
+	i = 0; // To iterate through frame
+	j = 0; // To iterate through ref string
+	for (j = 0; i < numFrames; j++) { 
+		if (refString[j] == -1) // If at the end of ref string, break
+			break;
+		// If the requested page is not in memory, load it
+		else if (!isLoaded(refString[j], memory, numFrames)) { 
+			memory[i] = refString[j];
+			i++;
+			printMem(memory, numFrames);
+		} // If in memory, just read it and continue
+		else {
+			printf("%d found in memory.\n", refString[j]);
+		}
+	}
+
+	while (j < refStringLen) {
+		int indexToReplace;
+		int outVal;
+		// If frame is loaded already, print
+		if (isLoaded(refString[j], memory, numFrames)) {
+			printf("%d found in memory.\n", refString[j]);
+		} else { 
+		// Begin page replacement algorithm
+			indexToReplace = 100;
+			int frameVal = -1;
+			// iterate through each frame
+			for (int k = 0; k < numFrames; k++) {
+				frameVal = memory[k];
+				// Go backwards from index to find least recently used
+				// Deterines least recently used by finding most previous
+				// call in the reference string and comparing to determine
+				// which was called earliest, or minimum position.
+				for (int l = (j-1); l >= 0; l--) {
+					if (frameVal == refString[l]) {
+						indexToReplace = min (indexToReplace, l);
+	//					printf("lowestInd: %d l: %d\n", indexToReplace,l);
+						break;
+					}
+				}
+			}
+			outVal = refString[indexToReplace];
+			for (int l = 0; l < numFrames; l++) {
+				if (memory[l] == outVal) {
+					memory[l] = refString[j];
+					break;
+				}
+			}
+			printMem(memory, numFrames);
+		}
+		j++;
+	}
+	printf("PAGE FAULTS: %d\n", faults);
 
 	return 0;
 }
